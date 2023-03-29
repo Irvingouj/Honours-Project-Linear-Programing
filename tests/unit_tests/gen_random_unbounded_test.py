4e699b33442d57821b25af696364ef3adeef3b35
JUNYI OU <49964599+Irvingouj@users.noreply.github.com>
2023-03-29 10:38:58 -0400

    random_gen_3d_unbound_seems_working

--- tests/unit_tests/gen_random_unbounded_test.py
+++ tests/unit_tests/gen_random_unbounded_test.py
@@ -11,6 +11,15 @@ class TestRandomGenUnbound(unittest.TestCase):
             obj, cons = gen.gen_random_2d_unbounded(i)
             solver = OsToolSolver()
             res = solver.solve(obj, cons)
-            if res is not None:
+            if res is not 'UNBOUNDED':
                 compare.write_bad_program((obj, cons), None, res, "suppose to be unbounded")
-            self.assertTrue(res == None)
+            self.assertTrue(res == 'UNBOUNDED')
+
+    def test_random_gen_unbounded_3d(self):
+        for i in range(100):
+            obj, cons = gen.gen_random_3d_unbounded(i*10)
+            solver = OsToolSolver()
+            res = solver.solve_three_d(obj, cons)
+            if res is not 'UNBOUNDED':
+                compare.write_bad_program((obj, cons), None, res, "suppose to be unbounded")
+            self.assertTrue(res == 'UNBOUNDED')
